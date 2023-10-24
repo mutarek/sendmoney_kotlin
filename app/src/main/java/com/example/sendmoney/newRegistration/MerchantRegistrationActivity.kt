eab729a826d4606df5a0b35dea6d4619a36181fd
tarek <mohiuddintarek@gmail.com>
2023-10-24 11:11:08 +0600

    merchant registration ui done

---
+++ app/src/main/java/com/example/sendmoney/newRegistration/MerchantRegistrationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sendmoney.newRegistration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sendmoney.R
+
+class MerchantRegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_merchant_registration)
+    }
+}
